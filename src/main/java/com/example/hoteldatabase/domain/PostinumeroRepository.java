cc1ffaaf04a53b21f6f9ba38aa1df545c831ff4d
Dovile Mart <doviliu@gmail.com>
2023-03-23 18:39:49 +0200

    Listan lisäys

--- src/main/java/com/example/hoteldatabase/domain/PostinumeroRepository.java
+++ src/main/java/com/example/hoteldatabase/domain/PostinumeroRepository.java
@@ -1,7 +1,10 @@
 package com.example.hoteldatabase.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostinumeroRepository extends CrudRepository<Postinumero, String>{
+	List<Postinumero> findByPostinumero(String postinumero);
 
 }
