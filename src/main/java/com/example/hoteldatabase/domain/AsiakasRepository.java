1225334938da6e4669a40a31173aa55b8ab3df44
Dovile Mart <doviliu@gmail.com>
2023-03-23 18:32:33 +0200

    Listan lisäys

--- src/main/java/com/example/hoteldatabase/domain/AsiakasRepository.java
+++ src/main/java/com/example/hoteldatabase/domain/AsiakasRepository.java
@@ -1,7 +1,10 @@
 package com.example.hoteldatabase.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface AsiakasRepository extends CrudRepository<Asiakas, Long>{
-
+	//List<Asiakas> findByEtunimi(String etunimi);
+	List<Asiakas> findByAsiakasId(Long asiakasId);
 }
