4bc4024b376a795b4ad74829055b9c2b11b30d81
Dovile Mart <doviliu@gmail.com>
2023-03-23 18:49:29 +0200

    GET asiakkaat-endpointi

--- src/main/java/com/example/hoteldatabase/web/AsiakasController.java
+++ src/main/java/com/example/hoteldatabase/web/AsiakasController.java
@@ -1,8 +1,22 @@
 package com.example.hoteldatabase.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.hoteldatabase.domain.Asiakas;
+import com.example.hoteldatabase.domain.AsiakasRepository;
 
 @Controller
 public class AsiakasController {
 
+	@Autowired
+	AsiakasRepository asiakasRepo;
+	
+	@GetMapping("/asiakkaat")
+	public String listAsiakkaat(Model model) {
+		model.addAttribute("asiakas", new Asiakas());
+		return "asiakkaat";
+	}
 }
