83828eb880c4d6faab1af8210de38ccf6e407bb4
Dovile Mart <doviliu@gmail.com>
2023-03-23 18:50:54 +0200

    tyontekija kontrolleri ja GET tyontekijat-endpointi

---
+++ src/main/java/com/example/hoteldatabase/web/TyontekijaController.java
@@ -0,0 +1,22 @@
+package com.example.hoteldatabase.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.hoteldatabase.domain.Tyontekija;
+import com.example.hoteldatabase.domain.TyontekijaRepository;
+
+@Controller
+public class TyontekijaController {
+
+	@Autowired
+	TyontekijaRepository tyontekijaRepo;
+	
+	@GetMapping("/tyontekijat")
+	public String listTyontekijat(Model model) {
+		model.addAttribute("tyontekija", new Tyontekija());
+		return "tyontekijat";
+	}
+}
