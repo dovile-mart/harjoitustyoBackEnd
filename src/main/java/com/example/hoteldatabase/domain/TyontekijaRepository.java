c7d08ed9baa40cf67210fc5c36fe919f9e80fc59
Dovile Mart <doviliu@gmail.com>
2023-03-30 23:33:50 +0300

    Listaus lisätty

--- src/main/java/com/example/hoteldatabase/domain/TyontekijaRepository.java
+++ src/main/java/com/example/hoteldatabase/domain/TyontekijaRepository.java
@@ -1,7 +1,9 @@
 package com.example.hoteldatabase.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface TyontekijaRepository extends CrudRepository<Tyontekija, Long>{
-
+	List<Tyontekija> findByTyontekijaId(Long tyontekijaId);
 }
