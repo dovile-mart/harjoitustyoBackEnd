95ddf502f2491fda4c31cce5f233a5c93fe6779e
Dovile Mart <doviliu@gmail.com>
2023-04-20 00:12:50 +0300

    Autorisointi ja postinumeron id lisätty Postmanin api/asiakkaat-testeihin

--- src/main/java/com/example/hoteldatabase/WebSecurityConfig.java
+++ src/main/java/com/example/hoteldatabase/WebSecurityConfig.java
@@ -20,20 +20,23 @@ public class WebSecurityConfig  {
 	private UserDetailServiceImpl userDetailsService;
 	
 	private static final AntPathRequestMatcher[] WHITE_LIST_URLS = {
-            new AntPathRequestMatcher("/api/**"),
+    //        new AntPathRequestMatcher("/api/**"),		//Postmanin testejä varten kommentoitu pois
             new AntPathRequestMatcher("/h2-console/**"),
-            new AntPathRequestMatcher("/index")
+            new AntPathRequestMatcher("/index"),
+            new AntPathRequestMatcher("/**")
     };
 	
 	private static final AntPathRequestMatcher[] ADMIN_LIST_URLS = {
-            new AntPathRequestMatcher("/admin/**")
-    };
+            new AntPathRequestMatcher("/admin/**"),
+            new AntPathRequestMatcher("/api/**")
+	};
 	
 	
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-		http
-		.authorizeHttpRequests().requestMatchers(ADMIN_LIST_URLS).hasAuthority("ADMIN")
+		http.httpBasic() // Postmania varten, ennen tätä Postman palauti html login sivun
+		.and()
+		.authorizeHttpRequests().requestMatchers(ADMIN_LIST_URLS).hasAuthority("ADMIN") //.hasRole("ADMIN")
 		.and()
 		.authorizeHttpRequests().requestMatchers(WHITE_LIST_URLS).permitAll()
 		.and()
@@ -44,9 +47,12 @@ public class WebSecurityConfig  {
 		.formLogin().defaultSuccessUrl("/index", true)
 		.and()
 		.logout().permitAll();
+		
+		http.cors().and().csrf().disable();
 		return http.build();
 
 	}
+
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
