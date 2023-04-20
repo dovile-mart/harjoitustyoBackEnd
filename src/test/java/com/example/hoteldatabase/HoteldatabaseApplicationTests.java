7cc9a1ef93b5e03b85d3d2084a27efe9736fa186
Dovile Mart <doviliu@gmail.com>
2023-04-20 18:29:53 +0300

    Testaus

--- src/test/java/com/example/hoteldatabase/HoteldatabaseApplicationTests.java
+++ src/test/java/com/example/hoteldatabase/HoteldatabaseApplicationTests.java
@@ -1,7 +1,11 @@
 package com.example.hoteldatabase;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.hoteldatabase.web.AsiakasController;
 
 @SpringBootTest
 class HoteldatabaseApplicationTests {
@@ -9,5 +13,13 @@ class HoteldatabaseApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Autowired
+	AsiakasController asiakasController;
+	@Test
+	public void contextLoadsAsiakasContr() throws Exception {
+		assertThat(asiakasController).isNotNull();
+	}
+	
 
 }
