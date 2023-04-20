738be1ba66b1a2c47a6c7fa083814be16b4982b3
Dovile Mart <doviliu@gmail.com>
2023-04-20 18:28:47 +0300

    koodin siivous

--- src/main/java/com/example/hoteldatabase/domain/HuoneRepository.java
+++ src/main/java/com/example/hoteldatabase/domain/HuoneRepository.java
@@ -5,10 +5,7 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface HuoneRepository extends CrudRepository<Huone, Long>{
-	//List<Huone> findById(Long id);
 	List<Huone> findByHuoneNro(String huoneNro);
 	List<Huone> findByHinta(int hinta);
 	List<Huone> findByOnkoVapaa(boolean onkoVapaa); //In addition, this class provides many methods forconverting a boolean to a String and a String to a boolean, as well as otherconstants and methods useful when dealing with a boolean. 
-//	List<Huone> findByHuonevarausId(Huonevaraus huonevarausId); //nope!!!!!!!
-//	List<Huone> findByHuonevaraus(Huonevaraus huonevaraus);//nope
 }
